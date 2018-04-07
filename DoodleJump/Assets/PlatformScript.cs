74073cd41a39d5057d56c22890ac3fb138c4a868
lip ables <phillipables1@gmail.com>
2018-04-07 18:53:47 -0400

    only run OnCollisionEnter code if relativeVelocity.y is less than zero

--- DoodleJump/Assets/PlatformScript.cs
+++ DoodleJump/Assets/PlatformScript.cs
@@ -8,15 +8,17 @@ public class PlatformScript : MonoBehaviour {
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        Rigidbody2D rb = collision.collider.GetComponent<Rigidbody2D>();
-        if (rb != null)
-        {
-            //getting the vector, modifying a component of the vector and then setting it back to this vector
-            Vector2 velocity = rb.velocity;
-            velocity.y = jumpForce;
-            rb.velocity = velocity;
-        }
-    
-            
+        if (collision.relativeVelocity.y <= 0f)
+        { 
+            Rigidbody2D rb = collision.collider.GetComponent<Rigidbody2D>();
+            if (rb != null)
+            {
+                //getting the vector, modifying a component of the vector and then setting it back to this vector
+                Vector2 velocity = rb.velocity;
+                velocity.y = jumpForce;
+                rb.velocity = velocity;
+            }
+
+        }   
     }
 }
