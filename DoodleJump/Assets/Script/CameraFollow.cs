8b87fb655536df452f79e6b14a32b61bdcd0a61a
lip ables <phillipables1@gmail.com>
2018-04-07 19:40:53 -0400

    on LateUpdate if doodler higher then camera then move camera to target height at smooth speed on a Lerp

--- DoodleJump/Assets/Script/CameraFollow.cs
+++ DoodleJump/Assets/Script/CameraFollow.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour {
-    public Transform target;
 
-    void Update()
+    public Transform target;
+    public float smoothSpeed = .3f;
+
+    //updates later than a normal update method, move char and then camera, no mixing b ack and forth
+    void LateUpdate()
     {
         if (target.position.y > transform.position.y)
         {
-            
+            Vector3 newPos = new Vector3(transform.position.x, target.position.y, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, newPos, smoothSpeed);
         }
     }
 }
