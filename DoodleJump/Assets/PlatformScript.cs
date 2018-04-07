ecf58956f19982a20e0658f90b6c533c5500bf01
lip ables <phillipables1@gmail.com>
2018-04-07 18:14:54 -0400

    public float for jumpForce, onCollisionEntrerr get rb of collider, if rb exist new vector2 equal to rb.velocity change the y to jump force and reset the velocity of the rb with the temp variable velocity

--- DoodleJump/Assets/PlatformScript.cs
+++ DoodleJump/Assets/PlatformScript.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class PlatformScript : MonoBehaviour {
 
+    public float jumpForce = 10f;
+
     void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        Rigidbody2D rb = collision.collider.GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            //getting the vector, modifying a component of the vector and then setting it back to this vector
+            Vector2 velocity = rb.velocity;
+            velocity.y = jumpForce;
+            rb.velocity = velocity;
+        }
+    
+            
     }
 }
