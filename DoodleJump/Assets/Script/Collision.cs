cf73d3e6c8305ed3c685145d36f1715399d2fec5
lip ables <phillipables1@gmail.com>
2018-04-08 18:04:49 -0400

    now can fly through bottom and not collide unless if from one direction up coming down

--- DoodleJump/Assets/Script/Collision.cs
+++ DoodleJump/Assets/Script/Collision.cs
@@ -3,39 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Collision : MonoBehaviour {
-    private SpriteRenderer spriteR;
+    private SpriteRenderer personSpriteR;
+    private SpriteRenderer carSpriteR;
     private Rigidbody2D rb;
-    private float jumpPOWA = 8f;
+    private float jumpPOWA = 11f;
 
     public Sprite crushed;
-    public Sprite car;
     public Sprite jump;
     public Sprite stand;
-
-
-    private void Start()
-    {
-        spriteR.sprite = car;
-        spriteR.sprite = crushed;
-        spriteR.sprite = jump;
-        spriteR.sprite = stand;
-    }
+    public Sprite car;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+
         //what data do you need that is being passed in?
-        if (collision.relativeVelocity.y < 0.0f)
+        if (collision.relativeVelocity.y < 0f)
         {
-            SpriteRenderer spriteR = gameObject.GetComponent<SpriteRenderer>();
+            SpriteRenderer carSpriteR = gameObject.GetComponent<SpriteRenderer>();
+            SpriteRenderer personSpriteR = collision.collider.GetComponent<SpriteRenderer>();
+            Rigidbody2D rb = collision.collider.GetComponent<Rigidbody2D>();
             //you still need to typecast it
             //what is colliding in the collision?
-            Rigidbody2D rb = collision.collider.GetComponent<Rigidbody2D>();
             if (rb != null)
             {
-                Debug.Log("BAmb");
-                spriteR.sprite = crushed;   
+
+                carSpriteR.sprite = crushed;
+                personSpriteR.sprite = stand;
+
+                Vector2 velocity = rb.velocity;
+                velocity.y = jumpPOWA;
+                rb.velocity = velocity;
             }
+            StartCoroutine("SpriteChangeDelay", collision);
+            //if (collision.gameObject.transform.position.y != 0)
         }
         
+    }
+    
+    private IEnumerator SpriteChangeDelay(Collision2D collision)
+    {
+        yield return new WaitForSeconds(0.13f);
+
+        Rigidbody2D rbCar = collision.gameObject.AddComponent<Rigidbody2D>();
+        SpriteRenderer personSpriteR = collision.collider.GetComponent<SpriteRenderer>();
+
+        personSpriteR.sprite = jump;
+        yield return null;
     }
 }
