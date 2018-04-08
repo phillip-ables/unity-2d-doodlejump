4764317cb00f0538778ad0167049a3c5e62c415a
lip ables <phillipables1@gmail.com>
2018-04-07 20:06:41 -0400

    on Start LevelGenerator new Vector3 spawnPosition for each numberOfPlatforms spawn at y random rang of min and max then instantiate our platform prefad at spawn position

--- DoodleJump/Assets/Script/LevelGenerator.cs
+++ DoodleJump/Assets/Script/LevelGenerator.cs
@@ -6,13 +6,18 @@ public class LevelGenerator : MonoBehaviour {
 
     public GameObject platformPrefab;
 
-    public int numberOfPlatforms;
+    public int numberOfPlatforms = 200;
     public float levelWidth = 3f;
     public float minY = .2f;
     public float maxY = 1.5f;
    
 	void Start () {
-
+        Vector3 spawnPosition = new Vector3();
+        for (int i = 0; i < numberOfPlatforms; i++)
+        {
+            spawnPosition.y += Random.Range(minY, maxY);
+            Instantiate(platformPrefab, spawnPosition, Quaternion.identity);
+        }
     }
 	
 	// Update is called once per frame
