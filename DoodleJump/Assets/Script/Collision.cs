5512ecb26fb67665c3bb63459e7486aa1cdf8589
lip ables <phillipables1@gmail.com>
2018-04-08 16:44:59 -0400

    car png switches to crushed after collision

---
+++ DoodleJump/Assets/Script/Collision.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour {
+    private SpriteRenderer spriteR;
+    private Rigidbody2D rb;
+    private float jumpPOWA = 8f;
+
+    public Sprite crushed;
+    public Sprite car;
+    public Sprite jump;
+    public Sprite stand;
+
+
+    private void Start()
+    {
+        spriteR.sprite = car;
+        spriteR.sprite = crushed;
+        spriteR.sprite = jump;
+        spriteR.sprite = stand;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //what data do you need that is being passed in?
+        if (collision.relativeVelocity.y < 0.0f)
+        {
+            SpriteRenderer spriteR = gameObject.GetComponent<SpriteRenderer>();
+            //you still need to typecast it
+            //what is colliding in the collision?
+            Rigidbody2D rb = collision.collider.GetComponent<Rigidbody2D>();
+            if (rb != null)
+            {
+                Debug.Log("BAmb");
+                spriteR.sprite = crushed;   
+            }
+        }
+        
+    }
+}
