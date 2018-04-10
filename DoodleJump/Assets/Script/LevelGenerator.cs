263e7c928f5234afc211d36017bf6269175de9ec
lip ables <phillipables1@gmail.com>
2018-04-10 15:32:57 -0400

    3 working classes of platforms

--- DoodleJump/Assets/Script/LevelGenerator.cs
+++ DoodleJump/Assets/Script/LevelGenerator.cs
@@ -4,7 +4,7 @@ public class LevelGenerator : MonoBehaviour {
 
     public GameObject platformPrefab;
     public GameObject jumpBoosterPrefab;
-    public GameObject hollowPrefab;
+    public GameObject badPrefab;
 
     public int numberOfPlatforms = 200;
     public float levelWidth = 3f;
@@ -13,25 +13,22 @@ public class LevelGenerator : MonoBehaviour {
    
 	void Start () {
         Vector3 spawnPosition = new Vector3();
+        Instantiate(platformPrefab, new Vector3(0, -2, 0), Quaternion.identity);
         for (int i = 0; i < numberOfPlatforms; i++)
         {
             spawnPosition.y += Random.Range(minY, maxY);
             spawnPosition.x = Random.Range(-levelWidth, levelWidth);
-            if(i%10 == 0)
+            if(i%11 == 0)
             {
                 Instantiate(jumpBoosterPrefab, spawnPosition, Quaternion.identity);
-            }else if(i%3 == 0)
+            }else if(i%7 == 0)
             {
-                hollowPrefab.transform.Rotate(new Vector3(180, 0, 0));
-                hollowPrefab.GetComponent<Collider2D>().enabled = false;
-                Instantiate(hollowPrefab, spawnPosition, Quaternion.identity);
+                Instantiate(badPrefab, spawnPosition, Quaternion.identity);
             }
             else
             {
                 Instantiate(platformPrefab, spawnPosition, Quaternion.identity);
-
             }
         }
-        Instantiate(platformPrefab, new Vector3(0, -2, 0), Quaternion.identity);
     }
 }
